af75cb55d8167727dce5752be2f808d1854a9ce8
lighter <zeke.kircheis@gmail.com>
2021-02-06 22:49:55 +0900

    fix template

--- src/module/Main.js
+++ src/module/Main.js
@@ -6,8 +6,10 @@ class Main extends React.Component{
   render (){
     return (
       <main id="main" className="main">
-        <h2 className="main__title">main title</h2>
-        <p className="main__txt">this is sample text. hello, everyone.</p>
+        <div className="contents">
+          <h2 className="main__title">main title</h2>
+          <p className="main__txt">this is sample text. hello, everyone.</p>
+        </div>
         <div className="contents">
           <Content />
           <Content />
