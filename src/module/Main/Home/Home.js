34792f95421924afa58b22af4b942a6bc4a1c32c
lighter <zeke.kircheis@gmail.com>
2021-02-08 14:54:27 +0900

    scss更新

--- src/module/Main/Home/Home.js
+++ src/module/Main/Home/Home.js
@@ -4,7 +4,10 @@ const Home = props => {
   return (
     <section className="home">
       <h2 className="home__title">What`s Project Ninja?</h2>
-      <p className="home_txt">忍者の隠れ里でござる。</p>
+      <p className="home_txt">
+        ひっそりとブログやツールを更新していくでござる。<br />
+        JS関係多めかも。
+      </p>
     </section>
   );
 }
