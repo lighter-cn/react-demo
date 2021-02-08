68382c5d031cc1a1c17ebc65498801fe2125ce26
lighter <zeke.kircheis@gmail.com>
2021-02-08 19:55:48 +0900

    scssでextendsを利用

--- src/module/Header/HeaderNav.js
+++ src/module/Header/HeaderNav.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React,{ useEffect } from "react";
 
 const HeaderNav = props => {
+  useEffect(() => {
+    const current = props.mode;
+    const nav = document.getElementsByClassName("header__nav");
+    for(let i = 0;i < nav[0].children.length;i++){
+      current === i ? nav[0].children[i].className = "header__nav-item_current" : nav[0].children[i].className = "header__nav-item";
+    }
+  });
+
   return (
   <ul className="header__nav">
     <li className="header__nav-item" onClick={()=>props.setMode(0)}>home</li>
