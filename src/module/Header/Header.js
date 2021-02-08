5a7913fc8a46be1a864266e94eb8740ce9f4af8f
lighter <zeke.kircheis@gmail.com>
2021-02-08 16:48:14 +0900

    関数コンポーネントに変更

--- src/module/Header/Header.js
+++ src/module/Header/Header.js
@@ -2,21 +2,19 @@ import React from 'react';
 import HeaderNav from "./HeaderNav";
 import HeaderToggle from "./HeaderToggle";
 
-class Header extends React.Component{
-  render(){
-    return(
-      <header id="header" className="header">
-        <div className="header__left">
-          <h1 className="header__title">Project Ninja</h1>
-          <p className="header__txt">Fun JavaScript</p>
-        </div>
-        <div className="header__right">
-          <HeaderNav />
-          <HeaderToggle />
-        </div>
-      </header>
-    );
-  }
+const Header = props => {
+  return(
+    <header id="header" className="header">
+      <div className="header__left">
+        <h1 className="header__title">Project Ninja</h1>
+        <p className="header__txt">Fun JavaScript</p>
+      </div>
+      <div className="header__right">
+        <HeaderNav />
+        <HeaderToggle />
+      </div>
+    </header>
+  );
 }
 
 export default Header;
