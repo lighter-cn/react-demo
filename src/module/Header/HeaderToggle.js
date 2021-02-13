d21f9e9885f991ba5f210ef9e93a418021217fc8
lighter <zeke.kircheis@gmail.com>
2021-02-13 21:52:09 +0900

    チェックボタンの挙動とCSS追加

--- src/module/Header/HeaderToggle.js
+++ src/module/Header/HeaderToggle.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 
 const HeaderToggle = props => {
+  const [isChecked,setChecked] = useState(true)
+
+  useEffect(()=>{
+    const s = document.getElementById("switch");
+    if(isChecked){
+      s.className="switch__btn";
+    }else{
+      s.className="switch__btn-off";
+    }
+  })
+
   return (
     <div className="header__toggle">
-        <button>toggle</button>
+      <label className="header__toggle-btn">
+        <div className="switch__base">
+          <div className="switch__btn" id="switch"></div>
+        </div>
+        <input
+          name="toggle"
+          type="checkbox"
+          checked={isChecked}
+          onChange={()=>{setChecked(!isChecked)}}
+          className="hidden"
+        />
+      </label>
     </div>
   );
 }
