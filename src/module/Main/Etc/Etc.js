279d0c702b0d7de36b9e2b1dbfc7ea7889406636
lighter <zeke.kircheis@gmail.com>
2021-02-14 22:44:41 +0900

    modify Etc.js

--- src/module/Main/Etc/Etc.js
+++ src/module/Main/Etc/Etc.js
@@ -3,8 +3,20 @@ import React from "react";
 const Etc = props => {
   return (
     <section className="etc">
-      <h2 className="etc__title">その他</h2>
+      <h2 className="etc__title">ETC</h2>
       <p className="etc_txt">コンタクトフォームなど</p>
+      <form className="contact">
+        <div className="contact__title">
+          <label>title:
+            <input type="text" name="title" className="contact__title-form"></input>
+          </label>
+        </div>
+        <div className="contact__text">
+          <label for="text">text:
+            <textarea name="text" className="contact__text-form"></textarea>
+          </label>
+        </div>
+      </form>
     </section>
   );
 }
