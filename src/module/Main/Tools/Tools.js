b01a123caa0d5b431eb9da481b04f623a414a6b2
lighter <zeke.kircheis@gmail.com>
2021-02-14 22:47:54 +0900

    Tools.scss 追加

--- src/module/Main/Tools/Tools.js
+++ src/module/Main/Tools/Tools.js
@@ -4,7 +4,7 @@ import Tool1 from "./Tool1";
 const Tools = props => {
   return(
     <section className="tools">
-      <h2 className="tools__title">Tools</h2>
+      <h2 className="tools__title">tools</h2>
       <p className="tools__txt">JavaScriptで自作したツール置き場</p>
       <Tool1 />
     </section>
