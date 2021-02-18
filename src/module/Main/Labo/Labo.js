b33b1e9428743783e728010c83af447c47244085
lighter <zeke.kircheis@gmail.com>
2021-02-18 15:54:15 +0900

    アニメーションの実験を追加

--- src/module/Main/Labo/Labo.js
+++ src/module/Main/Labo/Labo.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Labo1 from './Labo1'
 
 const Labo = props =>{
     return (
         <section className="labo">
             <h2 className="labo__title">labo</h2>
             <p classname="labo__txt">実験的なサンプル置き場</p>
+            <Labo1 />
         </section>
     );
 }
---
+++ src/module/Main/Labo/Labo1.js
@@ -0,0 +1,29 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+const Labo1 = () => {
+    return (
+        <div className="labo__contents">
+            <div className="labo__content">
+                <p className="labo__sub-title">
+                    animation1<br />
+                    change color
+                </p>
+                <div className="box_wrapper">
+                    <div className="box1"></div>
+                </div>
+            </div>
+            <div className="labo__content">
+                <p className="labo__sub-title">
+                    animation2<br />
+                    rolling box
+                </p>
+                <div className="box_wrapper">
+                    <div className="box2"></div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Labo1
