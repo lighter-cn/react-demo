160bd89f4d0b718e9dbb1f6ae29bcc807d51b118
lighter <zeke.kircheis@gmail.com>
2021-02-22 21:52:26 +0900

    jsonboxの投稿フォーム追加

--- src/module/Main/Tools/Tool1.js
+++ src/module/Main/Tools/Tool1.js
@@ -1,32 +1,48 @@
 import React, { useState,useEffect } from "react";
 
 const Tool1 = props => {
-  const [articles,setArticles] = useState({title: "foo",text: "bar"});
+  const [articles,setArticles] = useState(null);
 
   const getJson = (e) => {
     e.preventDefault();
-    console.log("push");
-    fetch("https://jsonbox.io/box_aa8304412bb017eaa234")
-    .then(res => {
-      console.log(res);
-    });
+    const title = document.getElementById('title').value;
+    const content = document.getElementById('content').value;
+
+    const obj = {title: title,content: content};
+    const method = "POST";
+    const body = JSON.stringify(obj);
+    const headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    };
+    fetch("https://jsonbox.io/box_aa8304412bb017eaa234", {method, headers, body}).then((res)=> res.json()).then(console.log).catch(console.error);
   }
 
-  // useEffect(()=>{
-  //   fetch("https://jsonbox.io/box_aa8304412bb017eaa234").then(res => console.log(res.json()))
-  // });
+  useEffect(()=>{
+    fetch("https://jsonbox.io/box_aa8304412bb017eaa234")
+    .then(res => res.json())
+    .then(res => {
+      for(const i in res){
+        console.log(res[i]);
+      }
+    })
+  });
 
   return (
     <section className="tool">
       <h2 className="tool__title">Tool1</h2>
       <p className="tool__txt">jsonの投稿と取得</p>
-      <table>
-        <th>title</th>
-        <th>text</th>
-        <td>{articles.title}</td>
-        <td>{articles.text}</td>
-      </table>
+      <p>{ articles }</p>
+
       <form>
+        <label>
+          Title:
+          <input type="text" name="title" id="title"/>
+        </label>
+        <label>
+          Content:
+          <textarea id="content"></textarea>
+        </label>
         <input type="submit" value="push" onClick={(e)=>{getJson(e)}}></input>
       </form>
     </section>
